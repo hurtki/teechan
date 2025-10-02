e2c87cd45e32d7f69c22e95316d4ba0965e787a8
uno/hurtki <asboba2101@gmail.com>
2025-10-02 13:17:51 +0300

    add ( comments and explanations for main.go functionality )

--- main.go
+++ main.go
@@ -9,29 +9,44 @@ import (
 )
 
 func main() {
+	// ch - some channel ( chan int ) 
+	ch := generator()
+	// our goal is to create from it two channels
+	// we are using a struct in /teechan/teechan.go
 	tch := teechan.NewTeeChan[int](2)
-	chs := tch.Execute(generator())
+	// giving to teechan a channel we need to "multiply"
+	chs := tch.Execute(ch)
+	// now we got two channels with the same outs as for first channel
+	
+	
+
 	wg := &sync.WaitGroup{}
 	wg.Add(2)
 
+	// now we are emulating to more gorutines that needed to read from the first channel 
+	// so now first gorutine uses chs[0]
+	// and second gorutine uses chs[1]
+	
 	go func() {
 		defer wg.Done()
 		for i := range chs[0] {
-			time.Sleep(time.Second * 2)
+			time.Sleep(time.Second)
 			fmt.Println("testing", i)
 		}
 	}()
+
 	go func() {
 		defer wg.Done()
 		for i := range chs[1] {
-			time.Sleep(time.Second)
 			fmt.Println("monitoring", i)
 		}
 	}()
+	
 	wg.Wait()
-
 }
 
+
+// generator() returns a channel and writes into three times numbers (0, 1, 2)
 func generator() chan int {
 	ch := make(chan int)
 	go func() {
